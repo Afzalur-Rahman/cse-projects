61b82475e05a98f732eb29bb809b0bc1c4beed8c
Afzalur-Rahman <73651048+Afzalur-Rahman@users.noreply.github.com>
2023-10-25 19:06:31 +0600

    working on css

--- home.php
+++ home.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 
+    <!-- swiper css link -->
+    <link
+  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"   />
+
     <!-- font awesome cdn link-->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
  
@@ -15,7 +19,38 @@
 
 </head>
 <body>
- 
+
+<!-- header section starts   -->
+<section  class="header">
+<a href="home.php" class="logo">travel </a>
+
+
+<nav class="navbar">
+<a href="home.php">home</a>
+<a href="about.php">about</a>
+<a href="package.php">package</a>
+<a href="book.php">book</a>
+
+
+
+</nav>
+
+</section>
+
+
+
+
+<!-- header section ends    -->
+
+
+
+
+
+ <!-- swiper js link  -->
+ <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+
+
    <!-- custom js file link  -->
 <script src="js/script.js"></script>
 
