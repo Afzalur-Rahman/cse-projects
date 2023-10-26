ea76f9be30f71c24daba36dc5353901c1ad8c1f2
Afzalur-Rahman <73651048+Afzalur-Rahman@users.noreply.github.com>
2023-10-27 00:44:01 +0600

    home contents

--- home.php
+++ home.php
@@ -35,6 +35,9 @@
 
 </nav>
 
+<div id="menu-btn" class="fas fa-bars"></div>
+
+
 </section>
 
 
@@ -43,8 +46,50 @@
 <!-- header section ends    -->
 
 
+<!-- footer starts here  -->
+
+<section class="footer">
+<div class="box-container">
+
+<div class="box">
+<h3>quick links</h3>
+<a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+<a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+<a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+<a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+</div>
+
+<div class="box">
+<h3>extra links</h3>
+<a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+<a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+<a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+<a href="#"> <i class="fas fa-angle-right"></i> terms of use</a> 
+</div>
+
+<div class="box">
+<h3>Contact info</h3>
+<a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+<a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+<a href="#"> <i class="fas fa-envelope"></i> afzals@gmail.com </a>
+<a href="#"> <i class="fas fa-map"></i> Dhaka,Bangladesh - 400104 </a>
+</div>
+
+<div class="box">
+<h3>follow us</h3>
+<a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+<a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+<a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+<a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+</div>
 
 
+
+</div>
+</section>
+
+
+<!-- foooter ends here    -->
 
  <!-- swiper js link  -->
  <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
