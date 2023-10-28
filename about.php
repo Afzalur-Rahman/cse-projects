c944743027a0cdd3ee501ccb1b969b65882119d5
khairul75 <107575587+khairul75@users.noreply.github.com>
2023-10-29 04:19:17 +0600

    Add files via upload

---
+++ about.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+</head>
+<body>
+    <section class="About">
+        <div class="Image">
+            <img src="image/about-img.jpg" alt="">
+</div>
+<section class="about">
+
+<div classe"image">
+
+<img src="images/about-Ing.jpg" alt="">
+
+</div>
+
+<div class="content">
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ture nunquam nulla iusto corporis dolor 
+commodi libero, vitae obcaecati optio rerum ab culpa nesciunt, earum mollitia quasi ipsam non. Aliquid, 
+ture.</p>
+
+<h3>why choose us?</i>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid rerum, delectus voluptate aliquam
+     quaerat iusto repellendus error nulla ab atque.</p>
+     div class="icon">
+
+<i class="fa fa-map"></i>
+
+<span>top destinations</span>
+
+</div>
+
+<div class="icon">
+
+<i class="fa fa-hand-holding-usd"></i>
+
+<span>affordable price</span>
+
+</div>
+
+<div class="icon">
+</div>
+
+<div class="icon">
+
+<i class"fas fa headset"></a>
+
+<span>24/7 guide service</span>
+
+</div>
+</div>
+</div>
+
+</body>
+</html>
