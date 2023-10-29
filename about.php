05b59372b9ad2215e392331d972c2bb996c9e40c
khairul75 <107575587+khairul75@users.noreply.github.com>
2023-10-29 12:38:18 +0600

    Update about.php

--- about.php
+++ about.php
@@ -3,59 +3,168 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us</title>
+    <title>About</title>
+    <link rel="stylesheet" href="styles.css">
+    <!-- swiper css link -->
+    <link
+  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"   />
+
+    <!-- font awesome cdn link-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+ 
+<!-- custom css file link -->
+<link rel="stylesheet" href="css/style.css">
+
+
+
 </head>
 <body>
-    <section class="About">
-        <div class="Image">
-            <img src="image/about-img.jpg" alt="">
+
+<!-- header section starts   -->
+<section  class="header">
+<a href="home.php" class="logo">travel </a>
+
+
+<nav class="navbar">
+<a href="home.php">home</a>
+<a href="about.php">about</a>
+<a href="package.php">package</a>
+<a href="book.php">book</a>
+
+
+
+</nav>
+
+<div id="menu-btn" class="fas fa-bars"></div>
+
+
+</section>
+
+
+
+
+<!-- header section ends    -->
+
+<section class="reviews">
+
+<div class="swiper reviews-slider">
+
+<div class="swiper-wrapper">
+<div class="slide">
+
+<div class="stars">
+
+<i class="fas fa-star"></i>
+
+<i class="fas fa-star"></i>
+
+<i class="fas fa-star"></>
+
+<i class="fas fa-star"></i>
+
+<i class="fa fa-star"></i>
 </div>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Doloribus deleniti corporis fugit fugiat, dolorem eos voluptatum ratione vero soluta excepturi?.</p>
+
+<h3>john deo</h3> 
+<span>traveler</span>
+
+<img src="images/pic-1.jpeg" alt="">
+</div>
+</div>
+</div>
+</section>
+
+
+<!-- footer starts here  -->
+
+<section class="footer">
+<div class="box-container">
+
+<div class="box">
+<h3>quick links</h3>
+<a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+<a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+<a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+<a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+</div>
+
+<div class="box">
+<h3>extra links</h3>
+<a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+<a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+<a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+<a href="#"> <i class="fas fa-angle-right"></i> terms of use</a> 
+</div>
+
+<div class="box">
+<h3>Contact info</h3>
+<a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+<a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+<a href="#"> <i class="fas fa-envelope"></i> afzals@gmail.com </a>
+<a href="#"> <i class="fas fa-map"></i> Dhaka,Bangladesh - 400104 </a>
+</div>
+
+<div class="box">
+<h3>follow us</h3>
+<a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+<a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+<a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+<a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+</div>
+
+
+
+</div>
+</section>
+
+
+<!-- foooter ends here    -->
+
+<div class="heading" style="background:url(images/about.jpeg) no-repeat"> 
+<h1>about us</h1>
+</div>
+
 <section class="about">
 
 <div classe"image">
 
-<img src="images/about-Ing.jpg" alt="">
+<img src="images/about-img.jpeg" alt="">
 
 </div>
 
 <div class="content">
-
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ture nunquam nulla iusto corporis dolor 
-commodi libero, vitae obcaecati optio rerum ab culpa nesciunt, earum mollitia quasi ipsam non. Aliquid, 
-ture.</p>
-
 <h3>why choose us?</h3>
-
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure nunquam nulla iusto corporis dolor 
+commodi libero, vitae obcaecati optio rerum ab culpa nesciunt, earum mollitia quasi ipsam non. Aliquid, 
+iure.</p>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid rerum, delectus voluptate aliquam
      quaerat iusto repellendus error nulla ab atque.</p>
-     div class="icon">
-
+<div class="icons-container">
+ <div class="icons">
 <i class="fa fa-map"></i>
 
 <span>top destinations</span>
 
 </div>
 
-<div class="icon">
+<div class="icons">
 
-<i class="fa fa-hand-holding-usd"></i>
+<i class="fas fa-hand-holding-usd"></i>
 
 <span>affordable price</span>
 
 </div>
 
-<div class="icon">
-</div>
+<div class="icons">
 
-<div class="icon">
-
-<i class"fas fa headset"></i>
+<i class="fas fa-headset"></i>
 
 <span>24/7 guide service</span>
 
 </div>
 </div>
 </div>
-
-</body>
-</html>
+</section>
